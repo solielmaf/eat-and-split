1d22d6fc0671c9e21340d5b00da8b975e7a8bff5
solielmaf <miraftsegaye19@gmail.com>
2024-10-31 22:33:11 +0300

    add

--- eat-and-split/src/App.js
+++ eat-and-split/src/App.js
@@ -1,8 +1,47 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {}
+function App() {
+  const [friendName, setFreindName] = useState();
+  const [friendImage, setFriendImage] = useState();
+
+  return (
+    <>
+      <AddFriend
+        setFreindName={setFreindName}
+        friendName={friendName}
+        setFriendImage={setFriendImage}
+      />
+
+      {/* <img
+        src={friendImage}
+        alt="profile"
+        style={{ width: "70PX", height: "70PX", borderRadius: "50%" }}
+  />*/}
+    </>
+  );
+}
 function FriendList() {}
-function AddFriend() {}
+function AddFriend({ friendName, setFreindName, setFriendImage }) {
+  return (
+    <>
+      <div>
+        👭 Freind Name{" "}
+        <input
+          type="text"
+          value={friendName}
+          onChange={(e) => setFreindName(e.target.value)}
+        />
+      </div>
+      🌆 Image URL{" "}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => setFriendImage(URL.createObjectURL(e.target.files[0]))}
+      />
+    </>
+  );
+}
 function SplitBill() {}
 
 export default App;
